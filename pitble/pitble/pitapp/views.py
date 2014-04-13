6743d7d2d3db238c773f30e7a546890302fa80b3
avara1986 <a.vara.1986@gmail.com>
2014-04-13 15:51:20 +0200

    add sing-in template and sing-in view

--- pitble/pitble/pitapp/views.py
+++ pitble/pitble/pitapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from django import forms
 
 # Create your views here.
 def index(request):
@@ -8,9 +8,24 @@ def index(request):
                               {},
                               context_instance=RequestContext(request))
 
-
-# Create your views here.
 def followers(request):
     return render_to_response('pitapp/followers.html',
                               {},
+                              context_instance=RequestContext(request))
+    
+class signInForm(forms.Form):
+    email = forms.CharField(max_length=100, required=True)
+    password = forms.PasswordInput()
+    
+def signIn(request):
+    if request.method == 'POST': 
+        form = signInForm(request.POST) 
+        if form.is_valid():
+            # LLamar al modelo para verificar si el usuario existe
+            pass 
+    else:
+        form = signInForm()
+        
+    return render_to_response('pitapp/sign-in.html',
+                              {'form': form,},
                               context_instance=RequestContext(request))
